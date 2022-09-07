ac1d8d6828c04a0e7812764f201d6c7b25ace42b
johnrdoty92 <johnrdoty92@users.noreply.github.com>
2022-09-07 11:30:06 +0000

    deploy: ec664e2ec694b29e02689d66bb413a288af318e4

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {open: true},
-  base: `/jubilee-production-sound/`,
-  plugins: [ react() ]
-});
