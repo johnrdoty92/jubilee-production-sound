153f8f9ea0be92170df22528822bdd55198735ce
John Doty <johnrdoty92@gmail.com>
2022-09-07 20:07:50 +0900

    add config for build

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {open: true},
+  base: `/jubilee-production-sound/`,
   plugins: [ react() ]
 });
